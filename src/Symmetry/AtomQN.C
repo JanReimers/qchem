d7522a34c89930e9c79c0b048631c5e731da92a3
Jan Reimers <janreimers@gmail.com>
2024-12-14 12:13:07 -0600

    Use containment instead of inheritence for angular part of AtomQN.

--- src/Symmetry/AtomQN.C
+++ src/Symmetry/AtomQN.C
@@ -3,33 +3,43 @@
 
 
 #include "Imp/Symmetry/AtomQN.H"
+#include "Imp/Symmetry/YlmQN.H"
+#include "Imp/Symmetry/OkmjQN.H"
 #include <iostream>
 #include <cassert>
 
 using std::cout;
 using std::endl;
 
-AtomQN::AtomQN(): YlmQN(0,0),n(0) {};
+AtomQN::AtomQN(): l(0),n(0),itsAngularQN(0) {};
 
-AtomQN::AtomQN(int _n, int _l, int _m) : YlmQN(_l,_m), n(_n) {};
+AtomQN::AtomQN(int _n, const YlQN& Yl) 
+: l(Yl.GetL()), n(_n+l), itsAngularQN(Yl.Clone()) {};
 
-AtomQN::AtomQN(int _n, const YlmQN& Ylm) : YlmQN(Ylm), n(_n+itsL) {};
-AtomQN::AtomQN(int _n, const YlQN& Yl) : YlmQN(Yl.GetL(),0), n(_n+itsL) {};
-
+AtomQN::AtomQN(int _n, const Omega_kmjQN& Okmj)
+: l(Okmj.Getl()), n(_n+l), itsAngularQN(Okmj.Clone()) {};
 
 bool AtomQN::Match(const QuantumNumber& qn) const
 {
     const AtomQN* yqn = dynamic_cast<const AtomQN*>(&qn);
     assert(yqn);
-    return n==yqn->n && itsL==yqn->itsL;
+    return n==yqn->n && l == yqn->l;
 }
 
+int AtomQN::GetDegeneracy() const
+{
+    return itsAngularQN->GetDegeneracy();
+}
+
+QuantumNumber* AtomQN::AddPrincipleQN(int index) const
+{
+    return itsAngularQN->AddPrincipleQN(index);
+}
 
 
 std::ostream& AtomQN::Write(std::ostream& os) const
 {
-    os << n;
-    return YlmQN::Write(os);
+    return os << n << *itsAngularQN;
 }
 
 std::istream& AtomQN::Read (std::istream& is)
