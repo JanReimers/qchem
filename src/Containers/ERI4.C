3fec54c9579ca2a9a948423ad4adec0d2cc2fa21
Jan Reimers <janreimers@gmail.com>
2025-01-28 09:57:04 -0600

    Support non-symmetric super matricies.
    We need these for off diagonal LS/SL exchange integrals.

--- src/Containers/ERI4.C
+++ src/Containers/ERI4.C
@@ -1,22 +1,26 @@
 #include "Imp/Containers/ERI4.H"
 
-ERI4::ERI4(size_t Nab, size_t Ncd) : itsData(Nab)
+template <class T,template<class> class M> ERI4T<T,M>::ERI4T(size_t Nab, size_t Ncd) : itsData(Nab,Nab)
 {
-    SMat Jcd(Ncd);
+    M<T> Jcd(Ncd,Ncd);
     Fill(Jcd,0.0);
     Fill(itsData,Jcd);
 }
 
-size_t ERI4::size() const
+template <class T,template<class> class M> size_t ERI4T<T,M>::size() const
 {
     size_t Nab=itsData.size();
     size_t Ncd=itsData(1,1).size();
     return Nab*Ncd;
 }
 
+template class ERI4T<double,SMatrix>;
+template class ERI4T<double, Matrix>;
+
 ERI4::SMat operator*(const ERI4& gabcd,const ERI4::SMat& Scd)
 {
-    ERI4::SMat Sab(gabcd.itsData.GetLimits());
+    //std::cout << "gabcd=" << gabcd.GetLimits() << " Scd=" << Scd.GetLimits() << std::endl;
+    ERI4::SMat Sab(gabcd.GetLimits());
     for (auto ia:Sab.rows())
         for (auto ib:Sab.cols(ia))
             Sab(ia,ib)=ERI4::contract(gabcd(ia,ib),Scd); //Dot(DirectMultiply(A,B))
@@ -25,7 +29,7 @@ ERI4::SMat operator*(const ERI4& gabcd,const ERI4::SMat& Scd)
 
 ERI4::SMat operator*(const ERI4::SMat& Sab, const ERI4& gabcd)
 {
-    ERI4::SMat Scd(gabcd.itsData(1,1).GetLimits());
+    ERI4::SMat Scd(gabcd(1,1).GetLimits());
     Fill(Scd,0.0);
     for (auto ia:Sab.rows())
     {
@@ -38,6 +42,7 @@ ERI4::SMat operator*(const ERI4::SMat& Sab, const ERI4& gabcd)
 
 double ERI4::contract(const ERI4::SMat& A,const ERI4::SMat& B)
 {
+    //std::cout << "ERI4::contract " << A.GetLimits() << " " << B.GetLimits() << std::endl;
     assert(A.GetLimits()==B.GetLimits());
     double ret=Dot(A.GetDiagonal(),B.GetDiagonal());
     for (auto ia:A.rows())
