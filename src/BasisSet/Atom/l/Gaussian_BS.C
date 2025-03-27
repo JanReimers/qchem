deada00a537e487a32cefb34f4d059ab2480c8ca
Jan Reimers <janreimers@gmail.com>
2025-03-27 12:20:10 -0600

    BS reorg 9

--- src/BasisSet/Atom/l/Gaussian_BS.C
+++ src/BasisSet/Atom/l/Gaussian_BS.C
@@ -3,8 +3,6 @@
 #include "Imp/BasisSet/Atom/l/Gaussian_BS.H"
 #include "Imp/BasisSet/Atom/l/Gaussian_IBS.H"
 #include "Imp/BasisSet/Atom/radial/Gaussian/ExponentScaler.H"
-#include "Imp/BasisSet/Atom/radial/Gaussian/Rk.H"
-#include "Imp/Integrals/AngularIntegrals.H"
 
 namespace Atoml
 {
@@ -18,29 +16,6 @@ BasisSet::BasisSet(size_t N, double emin, double emax, size_t LMax)
         Insert(new Orbital_IBS(this,gs.Get_es(L),L)); //Common with optr_vector     
 }
 
-void BS_Common::Insert(bs_t* bs)
-{
-    BasisSetImp::Insert(bs);
-    Append(bs);
-}
-
-const Cacheable* BS_Common::Create(size_t ia,size_t ic,size_t ib,size_t id) const
-{
-    return new ::Gaussian::RkEngine(unique_esv[ia]+unique_esv[ib],unique_esv[ic]+unique_esv[id],LMax(ia,ib,ic,id));
-}
-
-Vector<double>  BS_Common::loop_4_direct(size_t id, size_t la, size_t lc)  const
-{
-    const Cacheable* c=Cache4::loop_4(id);
-    const ::Gaussian::RkEngine* cd = dynamic_cast<const ::Gaussian::RkEngine*>(c);
-    return cd->Coulomb_Rk(la,lc);
-}
-Vector<double>  BS_Common::loop_4_exchange(size_t id, size_t la, size_t lc)  const
-{
-    const Cacheable* c=Cache4::loop_4(id);
-    const ::Gaussian::RkEngine* cd = dynamic_cast<const ::Gaussian::RkEngine*>(c);
-    return cd->ExchangeRk(la,lc);
-}
 
 
 } //namespace
---
+++ src/BasisSet/Atom/radial/Gaussian/BS_Common.C
@@ -0,0 +1,35 @@
+// File: Atom/radial/Gaussian/BS_Common.C  l/ml/kappa/mj independent part of BasisSet for atom Gaussians.
+
+#include "Imp/BasisSet/Atom/radial/Gaussian/BS_Common.H"
+#include "Imp/BasisSet/Atom/radial/Gaussian/Rk.H"
+#include <Irrep_BS.H>
+#include "oml/vector.h"
+
+namespace Gaussian
+{
+
+void BS_Common::Insert(bs_t* bs)
+{
+    BasisSetImp::Insert(bs);
+    Append(bs);
+}
+
+const Cacheable* BS_Common::Create(size_t ia,size_t ic,size_t ib,size_t id) const
+{
+    return new ::Gaussian::RkEngine(unique_esv[ia]+unique_esv[ib],unique_esv[ic]+unique_esv[id],LMax(ia,ib,ic,id));
+}
+
+Vector<double>  BS_Common::loop_4_direct(size_t id, size_t la, size_t lc)  const
+{
+    const Cacheable* c=Cache4::loop_4(id);
+    const ::Gaussian::RkEngine* cd = dynamic_cast<const ::Gaussian::RkEngine*>(c);
+    return cd->Coulomb_Rk(la,lc);
+}
+Vector<double>  BS_Common::loop_4_exchange(size_t id, size_t la, size_t lc)  const
+{
+    const Cacheable* c=Cache4::loop_4(id);
+    const ::Gaussian::RkEngine* cd = dynamic_cast<const ::Gaussian::RkEngine*>(c);
+    return cd->ExchangeRk(la,lc);
+}
+
+} //namespace
