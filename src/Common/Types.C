e43fe40cffe7cf6d3a22241bdba98c2f3e38c541
Jan Reimers <janreimers@gmail.com>
2025-08-02 17:14:38 -0600

    Add more types used of PW basis set.

--- src/Common/Types.C
+++ src/Common/Types.C
@@ -1,6 +1,7 @@
 // File: Types.C  Some common types used everywhere in the code
 module;
 // #include <cstddef>
+#include <complex>
 export module qchem.Types;
 export import oml.Vector3D;
 export import oml.Vector;
@@ -10,7 +11,11 @@ export using size_t = decltype(sizeof 0); //gcc-15-1 accepts this.
 // export using size_t; //gcc-15-1 rejects this.
 // export typedef std::size_t size_t; //gcc-15-1 rejects this.
 
+export using dcmplx=std::complex<double>;
+
 export typedef Vector3D<double> RVec3;
+export typedef Vector3D<dcmplx> CVec3;
 export typedef Vector3D<int>    IVec3;
 export typedef Vector3D<size_t> UVec3;
+
 export typedef Vector<double>   RVec;
