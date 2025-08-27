bd9df7e886c8f52b598c010f796b7c4268b7c0a6
Jan Reimers <janreimers@gmail.com>
2025-08-27 14:31:51 -0600

    Merge in SplineGrouper bug fix

--- src/BasisSet/Atom/radial/BSpline/Imp/SplineGrouper.C
+++ src/BasisSet/Atom/radial/BSpline/Imp/SplineGrouper.C
@@ -1,24 +1,28 @@
 // File: BasisSet/Atom/radial/BSpline/Imp/SplineGrouper.C
 module;
 #include <algorithm>
+#include <iostream>
 module qchem.BasisSet.Atom.Internal.SplineGrouper;
 import qchem.Types;
+using std::cout;
+using std::endl;
 
 template <size_t K> size_t SplineGrouper<K>::Insert(const spline_t& sp,size_t l)
 {
-    double rmin=sp.getSupport().front();
-    size_t index=unique_es.size();
-    if (const auto &ie =unique_es.find(rmin);ie==unique_es.end())
+    // double rmin=sp.getSupport().front();
+    size_t index=unique_sp.size();
+    if (const auto &ie =unique_sp.find(sp);ie==unique_sp.end())
     {
         // new exponent
         unique_spv.push_back(sp);
         maxls.push_back(l);
-        unique_es[rmin]=index;
+        unique_sp[sp]=index;
     }
     else 
         index=ie->second;
 
     if (l>maxls[index]) maxls[index]=l;
+    // cout << "SplineGrouper index,rmin,l,maxl=" << index << " " << rmin << " " << l << " " << maxls[index] << endl;
     return index;
 }
  
--- src/BasisSet/Atom/radial/BSpline/SplineGrouper.C
+++ src/BasisSet/Atom/radial/BSpline/SplineGrouper.C
@@ -6,17 +6,25 @@ module;
 export module qchem.BasisSet.Atom.Internal.SplineGrouper;
 export import qchem.BasisSet.Atom.Internal.ExponentGrouper;
 import qchem.Basisset.Atom.radial.BSpline.GLQuadrature;
+
 // 
 // We can use this class for splines by using the support window {rmin,rmax}.
-// For now just assume if two spline have the same rmin then they also have the same rmax
-// So just pass in rmin instead of the exponent.
 //
 // Keep a list of unique splines for a group BSpline irrep basis functions.
-// For each unique rmin also store an index and the maximum l angular momentum used
+// For each unique rmin,rmax also store an index and the maximum l angular momentum used
 // for that spline.  The idea is to share splines between different irreps 
 // and work out the radial integral tables up to LMax for each 4-way spline combination. 
 // This class should be working together with the charge distribution caching mechanism.
 //  
+template <class T, size_t K> struct cmpSplines1 {
+    bool operator()(const bspline::Spline<T,K>& a, const bspline::Spline<T,K>& b) const
+    {
+        double af=a.getSupport().front(), bf=b.getSupport().front();
+        if (af!=bf) return af<bf;
+        return a.getSupport().back()<b.getSupport().back();
+    }
+};
+
 export template <size_t K> class SplineGrouper : public Grouper
 {
     typedef bspline::Spline<double, K> spline_t;
@@ -26,5 +34,8 @@ public:
     //! Linear array of unique exponents.
     std::vector<spline_t> unique_spv; 
     std::map<size_t,const GLCache*> itsGLs;
+private: 
+    //Use this map instead of Grouper::unique_es.
+    std::map<spline_t,size_t,cmpSplines1<double,K>> unique_sp; //Unique splines.
 };
 
