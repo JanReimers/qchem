6e147652613d046b17783241a3db6f0b106d2646
Jan Reimers <janreimers@gmail.com>
2026-04-14 09:41:23 -0600

    Do something with MeshIntegrals.C

--- UnitTests/MeshIntegrals.C
+++ UnitTests/MeshIntegrals.C
@@ -1,25 +1,26 @@
 // File MeshIntegrals.C  Run through varios mesh types and parameters
 
 #include "gtest/gtest.h"
-
-#include "PolarizedGaussian/IntegralEngine.H"
-import qchem.LAParams;
 #include <iomanip>
-qchem.BasisSet.Molecule.PolarizedGaussian;
+#include <blaze/Math.h>
+import qchem.LAParams;
+import qchem.BasisSet;
+import qchem.BasisSet.Molecule.PolarizedGaussian;
 import qchem.Mesh.Integrator;
 import qchem.Atom;
 import qchem.Molecule;
 import qchem.Symmetry;
+import qchem.BasisSet.Molecule.PolarizedGaussian.Internal.Readers.Gaussian94;
 
 
 using std::cout;
 using std::endl;
 using std::setw;
 
-double norm1(const SMatrix<double>& m)
-{
-    return sqrt(Sum(DirectMultiply(m,m)));
-}
+// double norm1(const SMatrix<double>& m)
+// {
+//     return sqrt(Sum(DirectMultiply(m,m)));
+// }
 //----------------------------------------------------------------------------------------------
 //
 //  Testing class
@@ -28,10 +29,8 @@ class MeshIntegralsTests : public ::testing::Test
 {
 public:
     MeshIntegralsTests()
-    : Lmax(4   )
-    , Z(1)
-    , lap({qchem::Lapack,qchem::SVD,1e-6,1e-12})
-    , ie(new PolarizedGaussian::IntegralEngine())
+    : Z(5)
+    , reader("../../../BasisSetData/dzvp.bsd")
     , bs(0)
     , cl(new Molecule())
     {
@@ -42,63 +41,62 @@ public:
     void InitAtom()
     {
         cl->Insert(new Atom(Z,0.0,Vector3D(0,0,0)));
-        bs=new PolarizedGaussian::BasisSet(lap,3,.1,10.0,Lmax,cl);
+        bs=new PolarizedGaussian::BasisSet(&reader,cl);
     }
     
     void InitMolecule()
     {
         cl->Insert(new Atom(Z,0.0,Vector3D( 1.,0.,0.)));
         cl->Insert(new Atom(Z,0.0,Vector3D(-1.,0.,0.)));
-        bs=new PolarizedGaussian::BasisSet(lap,3,.1,10.0,Lmax,cl);        
+        bs=new PolarizedGaussian::BasisSet(&reader,cl);        
     }
     
-    int Lmax, Z;
-    LAParams lap;
-    AnalyticIE<double>* ie;
+    int Z;
+    PolarizedGaussian::Gaussian94Reader reader;
     PolarizedGaussian::BasisSet* bs;
     Cluster* cl;
 };
 
-TEST_F(MeshIntegralsTests, GaussAngles)
+TEST_F(MeshIntegralsTests, PolGaussianOverlap)
 {
     
     InitAtom();
-    cout << *bs << endl;
+    
     //cout.precision(2);
     size_t Nradial=100;
-    cout << "alpha m      s        p         d"  << endl;
-    for (size_t mMHL=2;mMHL<=4;mMHL++)
-    for (int aMHL=1;aMHL<=4;aMHL++)
+    double af=0.1;
+    cout << "alpha1     alpha2       m      norm     max(abs)"  << endl;
+    for (size_t mMHL=1;mMHL<=5;mMHL++)
     {
-        double alpha=0.5*aMHL;
-        MeshParams mp({qchem::MHL,Nradial,mMHL,alpha,qchem::Gauss,12,0,0});
-        MeshIntegrator<double> mi(cl->CreateMesh(mp));
-        for (auto ibs=bs->beginT();ibs!=bs->end();ibs++)
+        int a1=0,a2=0;
+        double min1=100.0,min2=100.0;
+        for (int aMHL=1;aMHL<=50;aMHL++)
         {
-            SMatrix<double> o= ie->MakeOverlap(*ibs);
-            SMatrix<double> on= mi.Overlap(**ibs);
-            double err=norm1(o-on);
-            cout << std::fixed << setw(3) << alpha << " " << setw(2) << mMHL << " " << setw(6) << log10(err) << ",   " ;
-//            EXPECT_NEAR(err,0.0,1e-7/Nradial);
+            double alpha=af*aMHL;
+            MeshParams mp({qchem::MHL,Nradial,mMHL,alpha,qchem::Gauss,12,0,0});
+            MeshIntegrator<double> mi(cl->CreateMesh(mp));
+            for (auto ibs:bs->Iterate<Real_OIBS>())
+            {
+                // cout << *ibs << endl;
+                rsmat_t delta= ibs->Overlap()-mi.Overlap(*ibs);
+                double err=norm(delta);
+                double merr=max(abs(delta));
+                if (err<min1)
+                {
+                    min1=err;
+                    a1=aMHL;
+                }
+                if (merr<min2)
+                {
+                    min2=merr;
+                    a2=aMHL;
+                }
+    //            EXPECT_NEAR(err,0.0,1e-7/Nradial);
+            }
+            
         }
-    cout << endl;
-        
+        cout << std::fixed << setw(3) << af*a1 << " " << af*a2 << " " << setw(2) << mMHL << " " << setw(6) << log10(min1) << " "<< setw(6) << log10(min2);
+        cout << endl;
     }
 }
 
-TEST_F(MeshIntegralsTests, GObritals)
-{
-    
-    InitMolecule();
-    cout << *bs << endl;
-    auto ibs=bs->beginT();
-    SMatrix<double> o= ie->MakeOverlap(*ibs);
-
-    MeshParams mp({qchem::MHL,100,3,2,qchem::GaussLegendre,12,4,3,3});
-    MeshIntegrator<double> mi(cl->CreateMesh(mp));
-    SMatrix<double> on= mi.Overlap(**ibs);
-   // cout << o << on << o-on << endl;
-    double err=norm1(o-on);
-    cout <<  std::setprecision(12) << err << endl ;
-    
-}
