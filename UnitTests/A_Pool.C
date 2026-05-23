ea694ea226d18d2de23e4ec5cefa3c15360a5848
Jan Reimers <janreimers@gmail.com>
2026-05-23 13:37:00 -0600

    Basis set pool 2: Slater/Extreme with unpol HF tests.

--- UnitTests/A_Pool.C
+++ UnitTests/A_Pool.C
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <iomanip>
 #include "BasisSetPool.H"
-// #include <blaze/Math.h>
 #include "gtest/gtest.h"
+#include "QchemTester.H"
+
+import qchem.Hamiltonian.Factory;
+import qchem.Factory;
+import qchem.Cluster;
+
 import qchem.LASolver;
 
 using std::cout;
@@ -18,13 +23,14 @@ public:
 
 TEST_F(BasisSetPoolTests,SlaterDisplay)
 {
-    for (size_t Z:{1,10,20,40,80,100})
+    for (size_t Z:{1,2,10,20,40,80,100})
     {
         cout << "---------------- Z=" << Z << " ---------------"<< endl;
         for (auto acc:{Low,Medium,High,Extreme})
         {
             rvec_t es=SlaterExponents(acc,Z);
-            cout << "N=" << es.size() << " {" << es[0] << "," << es[1] << " ... " << es[es.size()-1] << "}" << endl;
+            cout << BasisSetAccuracyStrs[static_cast<size_t>(acc)] << " " 
+                 << "N=" << es.size() << " {" << es[0] << "," << es[1] << " ... " << es[es.size()-1] << "}" << endl;
 
         }
     }
@@ -59,3 +65,34 @@ TEST_F(BasisSetPoolTests,SlaterOrthonoality)
         }
     }
 }
+
+
+
+using namespace qchem::Hamiltonian;
+class A_HF_U : public virtual QchemTester, public ::testing::TestWithParam<size_t>, TestAtom
+{
+public:
+    A_HF_U() : TestAtom(GetParam()) {};
+    virtual Hamiltonian* GetHamiltonian(cl_t& cluster) const
+    {
+        return Factory(Model::HF,Pol::UnPolarized,cluster);
+    }
+};
+
+TEST_P(A_HF_U,Slater_HFGroundStates_Extreme)
+{
+    size_t Z=GetParam();
+    cout << "---------------- Z=" << Z << " ---------------"<< endl;
+        
+    BasisSet::Real_BS* bs=Factory(Extreme,BasisSet::Atom::Type::Slater,Z); 
+    QchemTester::Init(1e-3,bs);
+    //       NMaxIter MinDeltaRo MinDelE MinVirial MinError StartingRelaxRo    MergeTol verbose
+    Iterate({   32     ,Z*1e-5    ,1e-7 , 1e-6      ,Z*1e-6 ,Z<40 ? 0.5 : 0.3   ,1e-7  ,true});
+    // cout << "RelativeHFError = " << RelativeHFError() << std::endl;
+    EXPECT_LT(RelativeHFError(),1e-6); 
+    EXPECT_TRUE(Converged()); 
+        
+}
+
+INSTANTIATE_TEST_SUITE_P(SlaterHFGroundStates,A_HF_U,::testing::Values(2,4,10,12,18,20,30,36,38,46,48,54,56,70,80,86,88));//)); 
+// INSTANTIATE_TEST_SUITE_P(SlaterHFGroundStates,A_HF_U,::testing::Values(2,70));//)); 
